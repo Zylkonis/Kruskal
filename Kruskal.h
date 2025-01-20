7f8a0e43b4a7343b0766b50ff17f25f4d7c5de6e
Teo Itsweire <teo.itsweire@etu.univ-poitiers.fr>
2025-01-20 13:30:06 +0100

    done

--- Kruskal.h
+++ Kruskal.h
@@ -4,9 +4,10 @@
 
 #ifndef KRUSKAL_H
 #define KRUSKAL_H
+
 #include <Graph.h>
 #include <string>
-
+#include <fstream>
 #include "Edge.h"
 
 class Kruskal {
@@ -78,13 +79,13 @@ public:
             parent.push_back(i);
         }
 
-        List<Edge> mst; // Résultat : arêtes de l'arbre couvrant minimal
+        List<Edge> acm; // Résultat : arêtes de l'arbre couvrant minimal
 
         // Parcours des arêtes triées
         for (int i = 0; i < edgesRes.get_size(); i++) {
             Edge edge = edgesRes[i];
             if (find(edge.getSrc()) != find(edge.getDest())) {
-                mst.push_back(edge); // Ajout de l'arête à l'arbre couvrant
+                acm.push_back(edge); // Ajout de l'arête à l'arbre couvrant
                 unionF(find(edge.getSrc()), find(edge.getDest())); // Fusion des ensembles
 
                 std::cout << "parent = {";
@@ -96,8 +97,127 @@ public:
         }
 
         std::cout << "affichage du res: " << std::endl;
-        for (int i = 0; i < mst.get_size(); i++) {
-            std::cout << "{" << mst[i].getSrc() << ", " << mst[i].getDest() << ", " << mst[i].getWeight() << "}" << std::endl;
+        for (int i = 0; i < acm.get_size(); i++) {
+            std::cout << "{" << acm[i].getSrc() << ", " << acm[i].getDest() << ", " << acm[i].getWeight() << "}" << std::endl;
+        }
+
+        resultKruscal(acm, resFile);
+    }
+
+    // Algorithme de Kruskal sur les matrices
+    void kruskalM(const std::string &graphFile, const std::string &resFile = "") {
+        const Graph graph = Graph::readGraphFromFile(graphFile);
+
+
+        // Création et ajout des arêtes
+        Matrix<int> edgesM = graph.getEdgesM();
+        for (int i = 0; i < graph.getOrder(); i++) {
+            for (int j = i; j < graph.getOrder(); j++) {
+                if (edgesM(i, j) != 0) {
+                    Edge e(i + 1, j + 1, edgesM(i, j));
+                    if (!isIn(e)) { // On ne veut pas de doublons
+                        edgesRes.push_back(e);
+                    }
+                }
+            }
+        }
+        std::cout << "affichage de lAdj: " << std::endl;
+        for (int i = 0; i < edgesRes.get_size(); i++) {   // Parcours des sommets
+            Edge e = edgesRes[i];
+            std::cout << "{" << e.getSrc() << ", " << e.getDest() << ", " << e.getWeight() << "}" << std::endl;
+        }
+
+
+        // Tri des arêtes par poids croissant
+        edgesRes.sort([](const Edge& a, const Edge& b) {
+            return a < b;
+        });
+        std::cout << "affichage de edgesRes: " << std::endl;
+        for (int i = 0; i < edgesRes.get_size(); i++) {
+            std::cout << "{" << edgesRes[i].getSrc() << ", " << edgesRes[i].getDest() << ", " << edgesRes[i].getWeight() << "}" << std::endl;
+        }
+
+        for (int i = 0; i <= graph.getOrder(); i++) {
+            parent.push_back(i);
+        }
+
+        List<Edge> acm; // Résultat : arêtes de l'arbre couvrant minimal
+
+        // Parcours des arêtes triées
+        for (int i = 0; i < edgesRes.get_size(); i++) {
+            Edge edge = edgesRes[i];
+            if (find(edge.getSrc()) != find(edge.getDest())) {
+                acm.push_back(edge); // Ajout de l'arête à l'arbre couvrant
+                unionF(find(edge.getSrc()), find(edge.getDest())); // Fusion des ensembles
+
+                std::cout << "parent = {";
+                for (int j = 0; j < parent.get_size(); j++) {
+                    std::cout << parent[j] << ", ";
+                }
+                std::cout << "}" << std::endl;
+            }
+        }
+
+        std::cout << "affichage du res: " << std::endl;
+        for (int i = 0; i < acm.get_size(); i++) {
+            std::cout << "{" << acm[i].getSrc() << ", " << acm[i].getDest() << ", " << acm[i].getWeight() << "}" << std::endl;
+        }
+
+        resultKruscal(acm, resFile);
+    }
+
+private:
+    void resultKruscal(List<Edge> acm, const std::string &resFile) {
+        bool connexe = true;
+        int par = 1;
+        while (connexe && par < parent.get_size() - 1) {
+            if (parent[par] != parent[par + 1]) {
+                connexe = false;
+            }
+            ++par;
+        }
+
+        int cost = 0;
+        for (int i = 0; i < acm.get_size(); i++) {
+            cost += acm[i].getWeight();
+        }
+
+        acm.sort([](const Edge& a, const Edge& b) {return a.getDest() < b.getDest(); ;});
+        acm.sort([](const Edge& a, const Edge& b) {return a.getSrc() < b.getSrc(); ;});
+
+        if (resFile.empty()) {
+            if (connexe) {
+                std::cout << "LE GRAPHE EST CONNEXE" << std::endl;
+            } else {
+                std::cout << "LE GRAPHE N'EST PAS CONNEXE" << std::endl;
+            }
+
+            std::cout << "Total cost: " << cost << std::endl;
+
+            std::cout << "affichage du res trie: " << std::endl;
+            for (int i = 0; i < acm.get_size(); i++) {
+                std::cout << "(" << acm[i].getSrc() << " -> " << acm[i].getDest() << " : " << acm[i].getWeight() << ")" << std::endl;
+            }
+        }
+        else {
+            std::ofstream file("../" + resFile);
+            if (file.is_open()) {
+                if (connexe) {
+                    file << "LE GRAPHE EST CONNEXE.\n";
+                } else {
+                    file << "LE GRAPHE N'EST PAS CONNEXE.\n";
+                }
+
+                file << "Coût total: " << cost << ".\n";
+
+                for (int i = 0; i < acm.get_size(); i++) {
+                    file << "(" << acm[i].getSrc() << " -> " << acm[i].getDest() << " : " << acm[i].getWeight() << ")\n";
+                }
+            } else {
+                std::cerr << "Impossible d'ouvrir le fichier." << std::endl;
+            }
+
+            file.close();
         }
     }
 };
--- main.cpp
+++ main.cpp
@@ -4,12 +4,19 @@
 
 #include "Kruskal.h"
 
-int main() {
-    std::cout << "Debut du programme" << std::endl;
+int main(int argc, char* argv[]) {
+    if (argc < 2 || argc > 3) {
+        std::cerr << "Nombre d'argument invalide." << std::endl;
+        return -1;
+    }
+    const std::string graphIn = argv[1];
 
-    const std::string graphFile = "testGraph";
+    const char* graphOut;
+    if (argc == 3) {
+        graphOut = argv[2];
+    }
     Kruskal k;
-    k.kruskalL(graphFile);
+    k.kruskalM(graphIn, graphOut);
 
     return 0;
 }
